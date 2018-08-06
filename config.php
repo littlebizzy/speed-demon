fb2835e8450e94cd86c4934e814c82ae369130a3
Pau <pauiglesias@gmail.com>
2018-08-06 17:06:15 +0200

    Starter and config scripts.

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+
+
+	'admin-notices-ms' => [
+
+		/**
+		 * Custom message
+		 * The %plugin% mark reflects the plugin name
+		 */
+		'message' => 'Sorry! For performance reasons, WordPress Multisite is not supported by <strong>%plugin%</strong>. Achieve top speed and security with a <a href="https://www.littlebizzy.com/hosting?utm_source=multisite" target="_blank">dedicated Nginx VPS</a> for every site.',
+
+	], // End of admin-notices-ms
+
+
+
+]; // End of main array
---
+++ speed-demon.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Plugin Name: Speed Demon
+Plugin URI: https://www.littlebizzy.com/plugins/speed-demon
+Description: A powerful bundle of lightweight tools and settings that drastically improve the loading speed of WordPress by reducing bulk and improving efficiency.
+Version: 1.0.0
+Author: LittleBizzy
+Author URI: https://www.littlebizzy.com
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+// Plugin namespace
+namespace LittleBizzy\SpeedDemon;
+
+// Aliased namespaces
+use LittleBizzy\SpeedDemon\Notices;
+
+// Block direct calls
+if (!function_exists('add_action'))
+	die;
+
+// Plugin constants
+const FILE = __FILE__;
+const PREFIX = 'spddmn';
+const VERSION = '1.0.0';
+
+// Loader
+require_once dirname(FILE).'/helpers/loader.php';
+
+/**
+ * Admin Notices Multisite check
+ * Uncomment "return;" to disable this plugin on Multisite installs
+ */
+if (false !== Notices\Admin_Notices_MS::instance(__FILE__)) { /* return; */ }
+
+// Run the main class
+Helpers\Runner::start('Core\Core', 'instance');
