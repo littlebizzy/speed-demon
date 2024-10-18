436163bc1ea6ed7459d3fe16509ef6d132a45242
Jesse Nickles <jesse@littlebizzy.com>
2024-10-18 19:42:31 +0700

    Update speed-demon.php

--- speed-demon.php
+++ speed-demon.php
@@ -2,41 +2,38 @@
 /*
 Plugin Name: Speed Demon
 Plugin URI: https://www.littlebizzy.com/plugins/speed-demon
-Description: A powerful bundle of lightweight tweaks that drastically improve the loading speed of WordPress by reducing bloat and improving overall efficiency.
-Version: 1.4.0
+Description: Performance hacks for WordPress
+Version: 2.0.0
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
-Prefix: SPDDMN
+GitHub Plugin URI: littlebizzy/speed-demon
+Primary Branch: master
 */
 
-// Plugin namespace
-namespace LittleBizzy\SpeedDemon;
+// Prevent direct access
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
 
-// Aliased namespaces
-use LittleBizzy\SpeedDemon\Notices;
+// Disable WordPress.org updates for this plugin
+add_filter( 'gu_override_dot_org', function( $overrides ) {
+    $overrides[] = 'speed-demon/speed-demon.php';
+    return $overrides;
+}, 999 );
 
-// Block direct calls
-if (!function_exists('add_action'))
-	die;
+// Load settings page
+require_once plugin_dir_path(__FILE__) . 'admin/settings-page.php';
 
-// Plugin constants
-const FILE = __FILE__;
-const PREFIX = 'spddmn';
-const VERSION = '1.4.0';
+// Load enabled modules
+$modules = get_option('speed_demon_enabled_modules', array());
 
-// Loader
-require_once dirname(FILE).'/helpers/loader.php';
+foreach ($modules as $module) {
+    $module_file = plugin_dir_path(__FILE__) . "modules/$module/$module.php";
+    if (file_exists($module_file)) {
+        require_once $module_file;
+    }
+}
 
-// Admin Notices
-Notices\Admin_Notices::instance(__FILE__);
-
-/**
- * Admin Notices Multisite check
- * Uncomment "return;" to disable this plugin on Multisite installs
- */
-if (false !== Notices\Admin_Notices_MS::instance(__FILE__)) { /* return; */ }
-
-// Run the main class
-Helpers\Runner::start('Core\Core', 'instance');
+// Ref: ChatGPT
